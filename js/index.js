7c8f003ccdbc58ce2c3d4f06d67b773354f1a3ef
mohammed-20 <ma7mmed.n3im@gmail.com>
2020-09-30 09:48:09 +0300

    setInterval

--- js/index.js
+++ js/index.js
@@ -7,52 +7,48 @@ const textinpt = document.getElementById('color');
 
 
 const getRandomNumber  = (min, max) => {
-    let step1 = max - min + 1;
-    let step2 = Math.random() * step1;
-    let result = Math.floor(step2) + min;
-    return result;
+     let step1 = max - min + 1;
+     let step2 = Math.random() * step1;
+     let result = Math.floor(step2) + min;
+      return result;
 };
 
-btnR.addEventListener('mouseenter' , () => {
-    let randomColor = getRandomNumber(0, colors.length-1);
-    document.getElementById('pallete').style.backgroundColor = colors[randomColor];
-    document.getElementById('color').value = colors[randomColor];
-    document.getElementById('color').style.color = colors[randomColor];
-    });
+    // btnR.addEventListener('mouseenter' , () => {
+    //   let randomColor = getRandomNumber(0, colors.length-1);
+    //     document.getElementById('pallete').style.backgroundColor = colors[randomColor];
+    //     document.getElementById('color').value = colors[randomColor];
+    //     document.getElementById('color').style.color = colors[randomColor];
+    // });
 
-    btnR.addEventListener('mouseleave' , () => {
-        let randomColor = getRandomNumber(0, colors.length-1);
-        document.getElementById('pallete').style.backgroundColor = colors[randomColor];
-        document.getElementById('color').value = colors[randomColor];
-        document.getElementById('color').style.color = colors[randomColor];
+    // btnR.addEventListener('mouseleave' , () => {
+    //   let randomColor = getRandomNumber(0, colors.length-1);
+    //     document.getElementById('pallete').style.backgroundColor = colors[randomColor];
+    //     document.getElementById('color').value = colors[randomColor];
+    //     document.getElementById('color').style.color = colors[randomColor];
+    //     });
+
+
+    textinpt.addEventListener('changecolor', () => {
+        document.getElementById("pallete").style.backgroundColor = document.getElementById('color').value;
         });
 
 
-        textinpt.addEventListener('changecolor', () => {
-        document.getElementById("pallete").style.backgroundColor = document.getElementById('color').value;
-        })
+let nIntervId;
+btnR.addEventListener('mouseenter', () =>{
+nIntervId=setInterval(set, 200);
+});
+
+const set = () =>{
+    let randomColor = getRandomNumber(0, colors.length-1);
+        document.getElementById('pallete').style.backgroundColor = colors[randomColor];
+        document.getElementById('color').value = colors[randomColor];
+        document.getElementById('color').style.color = colors[randomColor]; 
+        document.getElementById('color-text').style.color = colors[randomColor];
+        document.getElementById('btn1').style.backgroundColor =colors[randomColor];
+};
+
+btnR.addEventListener('mouseleave', () =>{
+    clearInterval(nIntervId);
+});
 
 
-//     let nIntervId;
-// const crazy = document.querySelector("#crazy");
-// function changeColor() {
-//     if (nIntervId) {
-//       clearInterval(nIntervId);
-//     }
-//     nIntervId = setInterval(()=>{
-//         let numRand =Math.floor( Math.random()*(colors.length));
-//     crazy.style.backgroundColor = colors[numRand];
-
-//     crazy.textContent=colors[numRand];
-//     }, 50);
-//   }
-
-
-//  crazy.addEventListener("click",changeColor);
-//  crazy.addEventListener("dblclick",stopTextColor);
-
-
-//  function stopTextColor() {
-//   clearInterval(nIntervId);
-//    crazy.removeEventListener("click",changeColor);
-// }
